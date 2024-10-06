75d0781be135654dcd68ff00ee3239a987c8c314
Aayushka g.c. <aayushkagc369@gmail.com>
2024-10-06 17:32:02 +0545

    first commit

---
+++ tmis-main/TMSI Home Page/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub SQlToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SQlToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub सटङToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles सटङToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub रपरटडजइनरToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles रपरटडजइनरToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub हमरबरमToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles हमरबरमToolStripMenuItem.Click
+
+    End Sub
+End Class
